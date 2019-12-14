ebccfc87c41bfa0846ffcb08f48e3353a4ea3417
Dag Stockstad <dag.stockstad@gmail.com>
2019-12-14 00:59:40 +0000

    Adding fading LED basic program
    ...

---
+++ irelandMap/irelandMap.ino
@@ -0,0 +1,32 @@
+int donegalLED = 11;
+int sligoLED = 12;
+int inishbofinLED = 13;
+
+int allLEDs[] = {donegalLED, sligoLED, inishbofinLED};
+int numberOfLEDs = 3;
+
+void setup() {
+  for (byte i = 0; i < numberOfLEDs; i = i + 1) {
+    pinMode(allLEDs[i], OUTPUT);
+  }
+}
+
+void lightLEDsWithBrightness(int brightness) {
+  for (int i = 0; i < numberOfLEDs; i = i + 1) {
+    analogWrite(allLEDs[i], brightness);
+  }
+}
+
+void loop() {
+  for (int brightness = 0; brightness < 255; brightness = brightness + 1) {
+    lightLEDsWithBrightness(brightness);
+    delay(10);
+  }
+  delay(1000);
+  
+  for (int brightness = 255; brightness >= 0; brightness = brightness - 1) {
+    lightLEDsWithBrightness(brightness);
+    delay(10);
+  }
+  delay(1000);
+}
